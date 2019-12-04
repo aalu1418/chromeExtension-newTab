5d5aaa2e156c9c15a447d78f794d46302d5103f9
aalu1418 <aalu1418@gmail.com>
2019-12-04 16:24:59 -0500

    added clock & day functionality, is able to pull data from geocoding api and store/retrieve in local storage

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ Things to include:
 - Time
 - Weather
 	- https://darksky.net/dev/docs#forecast-request
+	- [geocoding](https://developers.google.com/maps/documentation/geocoding/start)
 - Presto Card Balance
 	- https://stackoverflow.com/questions/44269313/how-to-check-presto-card-balance-without-an-account-using-only-python-requests
 	- https://medium.com/@bretcameron/how-to-build-a-web-scraper-using-javascript-11d7cd9f77f2
+
+---
+Notes:
+- [storing api keys](https://medium.com/better-programming/using-environment-variables-in-reactjs-9ad9c5322408)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import moment from "moment";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-Content">
+        <Clock />
+        <Weather />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+const Clock = () => {
+  const [time, setTime] = React.useState(null);
+  const [date, setDate] = React.useState(null);
+
+  React.useEffect(() => {
+    const clock = setInterval(() => {
+      setTime(moment().format("hh mm ss"));
+      setDate(moment().format("dddd, DD MMMM YYYY"));
+    }, 1000);
+
+    return () => {
+      clearInterval(clock);
+    };
+  }, []);
+
+  return (
+    <div className="Clock">
+      <div className="Clock-Time">{time}</div>
+      <div className="Clock-Date">{date}</div>
+    </div>
+  );
+};
+
+const Weather = () => {
+  const [location, setLocation] = React.useState(null);
+  const [locationData, setLocationData] = React.useState(
+    JSON.parse(localStorage.getItem("locationData"))
+  );
+
+  React.useEffect(() => {
+    if (location) {
+      fetch(
+        "https://us1.locationiq.com/v1/search.php?key=" +
+          process.env.REACT_APP_LOCATION_KEY +
+          "&q=" +
+          location +
+          "&format=json"
+      )
+        .then(data => data.json())
+        .then(response => {
+          setLocationData(response);
+          localStorage.setItem("locationData", JSON.stringify(response));
+        });
+    }
+  }, [location]);
+
+  const enterPress = event => {
+    if (event.key === "Enter") {
+      setLocation(event.target.value);
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" onKeyDown={enterPress} />
+    </div>
+  );
+};

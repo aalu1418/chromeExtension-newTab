59b3b870efdb55237ab12959c6b2944af1abb576
aalu1418 <aalu1418@gmail.com>
2020-02-13 18:52:30 -0500

    switched to useeffect for background color - fixes #1

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import "./App.css";
 import { readLocalStorage } from "./components/updateLocalStorage";
 
 const App = () => {
-  const [backgroundColor, setBackgroundColor] = React.useState(
-    readLocalStorage("color") || "black"
-  );
+  const [backgroundColor, setBackgroundColor] = React.useState();
   const color = Color(backgroundColor).isDark() ? "white" : "black";
   const [screen, setScreen] = React.useState(false);
   const screenState = { screen, setScreen };
   const [unit, setUnit] = React.useState(readLocalStorage("unit") || "si");
   const unitState = { unit, setUnit };
+
+  React.useEffect(() => {
+    setBackgroundColor(readLocalStorage("color") || "black");
+  }, [])
 
   return (
     <div className="App">
